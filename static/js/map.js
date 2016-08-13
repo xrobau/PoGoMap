b610265cb97ea72e1d3e3a72cd285263977776a3
James <james@studio-xp.com>
2016-08-13 14:15:44 -0400

    Removed trailing space

--- static/js/map.js
+++ static/js/map.js
@@ -1574,7 +1574,7 @@ function sendNotification (title, text, icon, lat, lng) {
       onClick: function () {
         window.focus()
         this.close()
-        
+
         centerMap(lat, lng, 20)
       }
     })
